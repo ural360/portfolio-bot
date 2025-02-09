4ab7313f863fbba07b62510d58d0fa9647b7310a
ural360 <75667422+ural360@users.noreply.github.com>
2025-02-09 11:55:34 +0500

    Add files via upload

--- logic.py
+++ logic.py
@@ -7,7 +7,7 @@ statuses = [ (_,) for _ in (['На этапе проектирования', 'В
 class DB_Manager:
     def __init__(self, database):
         self.database = database
-        
+
     def create_tables(self):
         conn = sqlite3.connect(self.database)
         with conn:
@@ -35,20 +35,22 @@ class DB_Manager:
                             status_name TEXT
                         )''')
             conn.commit()
+        print("База данных успешно создана.")
 
     def __executemany(self, sql, data):
         conn = sqlite3.connect(self.database)
         with conn:
             conn.executemany(sql, data)
             conn.commit()
-    
+
     def __select_data(self, sql, data = tuple()):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
             cur.execute(sql, data)
             return cur.fetchall()
-        
+            
+
     def default_insert(self):
         sql = 'INSERT OR IGNORE INTO skills (skill_name) values(?)'
         data = skills
@@ -59,26 +61,22 @@ class DB_Manager:
 
 
     def insert_project(self, data):
-        sql = """INSERT INTO projects 
-(user_id, project_name, url, status_id) 
-values(?, ?, ?, ?)"""
+        sql = 'INSERT OR IGNORE INTO projects (user_id, project_name, url, status_id) values(?, ?, ?, ?)'
         self.__executemany(sql, data)
-
 
     def insert_skill(self, user_id, project_name, skill):
         sql = 'SELECT project_id FROM projects WHERE project_name = ? AND user_id = ?'
         project_id = self.__select_data(sql, (project_name, user_id))[0][0]
         skill_id = self.__select_data('SELECT skill_id FROM skills WHERE skill_name = ?', (skill,))[0][0]
-        data = [(project_id, skill_id)]
-        sql = 'INSERT OR IGNORE INTO project_skills VALUES(?, ?)'
+        data = [(project_id,skill_id)]
+        sql = 'INSERT OR IGNORE INTO project_skills VALUES (?, ?)'
         self.__executemany(sql, data)
 
-
+  
     def get_statuses(self):
-        sql="SELECT status_name from status"
+        sql='SELECT status_name from status'
         return self.__select_data(sql)
         
-
     def get_status_id(self, status_name):
         sql = 'SELECT status_id FROM status WHERE status_name = ?'
         res = self.__select_data(sql, (status_name,))
@@ -86,13 +84,11 @@ values(?, ?, ?, ?)"""
         else: return None
 
     def get_projects(self, user_id):
-        sql="""SELECT * FROM projects 
-WHERE user_id = ?"""
-        return self.__select_data(sql, data = (user_id,))
-        
+        return self.__select_data(sql='SELECT * FROM projects WHERE user_id = ?', data = (user_id,))
+
     def get_project_id(self, project_name, user_id):
         return self.__select_data(sql='SELECT project_id FROM projects WHERE project_name = ? AND user_id = ?  ', data = (project_name, user_id,))[0][0]
-        
+
     def get_skills(self):
         return self.__select_data(sql='SELECT * FROM skills')
     
@@ -111,24 +107,22 @@ status.status_id = projects.status_id
 WHERE project_name=? AND user_id=?
 """
         return self.__select_data(sql=sql, data = (project_name, user_id))
-
+    
 
     def update_projects(self, param, data):
-        sql = f"""UPDATE projects SET {param} = ? 
-WHERE project_name = ? AND user_id = ?"""
-        self.__executemany(sql, [data]) 
+        self.__executemany(f"UPDATE projects SET {param} = ? WHERE project_name = ? AND user_id = ?", [data]) # data ('atr', 'mew', 'name', 'user_id')
 
 
     def delete_project(self, user_id, project_id):
-        sql = """DELETE FROM projects 
-WHERE user_id = ? AND project_id = ? """
+        sql = "DELETE FROM projects WHERE user_id = ? AND project_id = ? "
         self.__executemany(sql, [(user_id, project_id)])
-    
+
     def delete_skill(self, project_id, skill_id):
-        sql = """DELETE FROM skills 
-WHERE skill_id = ? AND project_id = ? """
+        sql = "DELETE FROM skills WHERE skill_id = ? AND project_id = ? "
         self.__executemany(sql, [(skill_id, project_id)])
 
 
 if __name__ == '__main__':
     manager = DB_Manager(DATABASE)
+    manager.create_tables()
+    manager.default_insert()
